31f6f98fb1452708b511b80a5704fb6ae41af048
Selim Reza <selimrezael@gmail.com>
2016-08-26 12:24:49 +0600

    devise added and routed root properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :podcasts
   resources :podcasts
 
-  root 'welcome#index'
+  authenticated do
+    root 'podcasts#index'
+  end
+
+  unauthenticated do
+    root 'welcome#index'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
